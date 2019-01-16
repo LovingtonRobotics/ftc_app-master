6c8df77a2135b308a7f931a7f747fb274334be30
LovingtonRobotics <46681943+LovingtonRobotics@users.noreply.github.com>
2019-01-16 14:46:28 -0700

    Add files via upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePad.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
 
 @TeleOp
@@ -11,21 +12,22 @@ public class GamePad extends OpMode {
    double leftwheelpower;
    double rightwheelpower;
 
-   double leftwheelpowernegative;
-   double rightwheelpowernegative;
-
 
    double armuppower;
    double armdownpower;
+
+   ColorSensor Color_sensor;
 
     @Override
     public void init() {
 
     robot.init(hardwareMap);
 
-        robot.Robot_Latch.setPosition(.45);
-        robot.Robot_Release.setPosition(0);
-        robot.Robot_Open.setPosition(1);
+    Color_sensor = hardwareMap.colorSensor.get("color");
+
+    robot.Robot_Latch.setPosition(.45);
+    robot.Robot_Release.setPosition(0);
+    robot.Robot_Open.setPosition(1);
 
     }
 
@@ -35,10 +37,6 @@ public class GamePad extends OpMode {
         leftwheelpower = gamepad1.right_stick_y;
         rightwheelpower = gamepad1.left_stick_y;
 
-        leftwheelpowernegative = -gamepad1.left_stick_y;
-        rightwheelpowernegative = -gamepad1.left_stick_y;
-
-
 
         armuppower = gamepad1.right_trigger;
         armdownpower = gamepad1.left_trigger;
@@ -46,14 +44,6 @@ public class GamePad extends OpMode {
         robot.Left_Wheel.setPower(leftwheelpower);
         robot.Right_Wheel.setPower(rightwheelpower);
 
-        if (gamepad1.dpad_down ){
-            robot.Left_Wheel.setPower(leftwheelpowernegative);
-            robot.Right_Wheel.setPower(rightwheelpowernegative);
-        }
-        else if (gamepad1.dpad_up){
-            robot.Left_Wheel.setPower(leftwheelpower);
-            robot.Right_Wheel.setPower(rightwheelpower);
-        }
 
         if (gamepad1.x) {
             robot.Robot_Latch.setPosition(0.45);
@@ -64,9 +54,11 @@ public class GamePad extends OpMode {
 
         if (gamepad1.a) {
             robot.Robot_Release.setPosition(.5);
+            Color_sensor.enableLed(true);  // turns on led when pin goes in
         }
         else if (gamepad1.b){
             robot.Robot_Release.setPosition(0);
+            Color_sensor.enableLed(false);  // Turns off led when pin is pulled out
         }
 
 
